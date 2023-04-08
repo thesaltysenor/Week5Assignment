5833b4c1179e2c75a8df518c970ed94f0e323c22
thesaltysenor <126297489+thesaltysenor@users.noreply.github.com>
2023-04-08 18:04:06 -0500

    Changed Strings in App file

--- src/App.java
+++ src/App.java
@@ -6,14 +6,14 @@ public class App {
         // Testing AsteriskLogger
         System.out.println("AsteriskLogger");
         asteriskLogger.log("Hello");
-        asteriskLogger.error("Hello");
+        asteriskLogger.error("Galaxy");
 
         //This is to separate a line on the console so you don't blend
         System.out.println();
 
         // Testing SpacedLogger
         System.out.println("SpacedLogger");
-        spacedLogger.log("Hello");
-        spacedLogger.error("Hello");
+        spacedLogger.log("Goodbye");
+        spacedLogger.error("Universe");
     }
 }
